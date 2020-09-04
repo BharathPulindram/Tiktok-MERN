3b914c508b45cbd9c5f133dcac4438c46005eff1
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-04 10:50:23 +0530

    Updates

---
+++ precache-manifest.fdc26c8ed1c9614acef35a649755b1f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5619624f347c5c2fff6b748ee5a376c0",
+    "url": "/Tiktok-MERN/index.html"
+  },
+  {
+    "revision": "6614d4297059c8da3311",
+    "url": "/Tiktok-MERN/static/css/main.2a68633e.chunk.css"
+  },
+  {
+    "revision": "0f1252081a31d1e350e3",
+    "url": "/Tiktok-MERN/static/js/2.b0323b82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tiktok-MERN/static/js/2.b0323b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6614d4297059c8da3311",
+    "url": "/Tiktok-MERN/static/js/main.4a193459.chunk.js"
+  },
+  {
+    "revision": "22e796ca42fd98f0b393",
+    "url": "/Tiktok-MERN/static/js/runtime-main.9505b645.js"
+  }
+]);
